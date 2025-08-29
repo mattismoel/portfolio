141d9e05eeb44b1f358bef3d417cd8c364269b4f
Mattis Kristensen <mattismoel@gmail.com>
2025-08-29 14:48:42 +0200

    update tab selector implementation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,14 +8,19 @@
   } from "$lib/project";
   import TechProjectList from "$lib/components/TechProjectList.svelte";
   import GraphicProjectList from "$lib/components/GraphicProjectList.svelte";
+  import TabSelector from "$lib/components/TabSelector.svelte";
 
-  type TabType = "tech" | "graphic-design";
+  const tabNames = ["Tech", "Graphic Design"] as const;
+  type TabType = (typeof tabNames)[number];
 
-  let tabType = $state<TabType>("graphic-design");
-  const tabs = new Map<TabType, string>([
-    ["tech", "Tech"],
-    ["graphic-design", "Graphic Design"],
-  ]);
+  const tabs: TabType[] = ["Tech", "Graphic Design"];
+  let selectedTab = $state<TabType>("Tech");
+
+  const isValidTabName = (tabName: string): tabName is TabType => {
+    const validTab = tabNames.find((n) => n === tabName);
+    if (!validTab) return false;
+    return true;
+  };
 
   let techPs = $derived(sortProjectsByFinishDate(techProjects));
   let graphicPs = $derived(sortProjectsByFinishDate(graphicsProjects));
@@ -29,31 +34,28 @@
         Hi! My name is {info.firstName}.
       </span>
       <br /><br />
-      I am an aspiring software developer from Odense, Denmark. This is my personal
+      I am an aspiring software developer from Odense, Denmark. This is a personal
       hub for all my projects - both finished, and in progress.
       <br /><br />
       Look around, and please do peek into the projects, and let me know if you find
       something of interest!
     </p>
   </section>
-  <section class="flex-1 flex-col">
-    <h1 class="text-2xl mb-10 font-bold text-heading" id="projects">
-      "Well, what have you been up to?", you ask...
-    </h1>
+  <section class="flex flex-1 flex-col gap-4">
+    <!-- <h1 class="text-2xl mb-10 font-bold text-heading" id="projects"> -->
+    <!--   "Well, what have you been up to?", you ask... -->
+    <!-- </h1> -->
 
-    <div class="w-full flex gap-2 mb-8">
-      {#each tabs as [t, tabName]}
-        <button
-          class:active={tabType === t}
-          class="w-full py-1 rounded-sm border border-transparent bg-zinc-950 hover:border-zinc-900 [.active]:font-bold [.active]:bg-zinc-900 transition-colors"
-          onclick={() => (tabType = t)}>{tabName}</button
-        >
-      {/each}
-    </div>
+    <TabSelector
+      {tabs}
+      onSelect={(newTab) =>
+        (selectedTab = isValidTabName(newTab) ? newTab : "Design")}
+      selected={selectedTab}
+    />
 
-    {#if tabType === "tech"}
+    {#if selectedTab === "Tech"}
       <TechProjectList projects={techPs} />
-    {:else if tabType === "graphic-design"}
+    {:else if selectedTab === "Graphic Design"}
       <GraphicProjectList projects={graphicPs} />
     {/if}
   </section>
