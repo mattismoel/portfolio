41af8c8d5fcdcd5b94b435d94ba0b591c4859d86
Mattis Kristensen <mattismoel@gmail.com>
2025-08-29 14:50:56 +0200

    add contacts page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,6 +1,17 @@
 <script lang="ts">
 </script>
 
-<main class="py-32 px-auto">
-  <h1></h1>
+<main class="py-32 px-auto min-h-svh flex flex-col">
+  <div class="flex-1 flex flex-col justify-center items-center gap-4">
+    <span>Catch me here</span>
+    <div class="flex flex-col items-center gap-2">
+      <a
+        href="mailto:mattismoel@gmail.com"
+        class="hover:underline hover:text-heading">mattismoel@gmail.com</a
+      >
+      <a href="tel:+4542157066" class="hover:underline hover:text-heading"
+        >+4542157066</a
+      >
+    </div>
+  </div>
 </main>
