7b986e630df4b82fef7fbb25823280d91009dccf
Mattis Kristensen <mattismoel@gmail.com>
2025-09-01 16:16:24 +0200

    add background to tab selector

--- frontend/src/lib/components/TabSelector.svelte
+++ frontend/src/lib/components/TabSelector.svelte
@@ -1,25 +1,27 @@
 <script lang="ts">
-  type Props = {
-    tabs: string[];
-    selected: string;
-    onSelect: (tab: string) => void;
-  };
+	type Props = {
+		tabs: string[];
+		selected: string;
+		onSelect: (tab: string) => void;
+	};
 
-  let { tabs, selected, onSelect }: Props = $props();
+	let { tabs, selected, onSelect }: Props = $props();
 
-  let selectedIdx = $derived(tabs.indexOf(selected));
+	let selectedIdx = $derived(tabs.indexOf(selected));
 </script>
 
 <div class="isolate relative w-full flex mb-8">
-  <div
-    style:transform="translateX(calc({selectedIdx}*100%))"
-    class="-z-10 absolute w-1/2 h-full bg-heading rounded-full transition-[transform] ease-in-out duration-100"
-  ></div>
-  {#each tabs as tab}
-    <button
-      class:active={selected === tab}
-      class="w-full py-1 cursor-pointer [.active]:cursor-default not-[.active]:hover:text-heading not-[.active]:hover:underline [.active]:font-bold [.active]:text-zinc-950 transition-colors duration-300"
-      onclick={() => onSelect(tab)}>{tab}</button
-    >
-  {/each}
+	<div class="-z-20 absolute bg-zinc-900 w-full h-full rounded-full"></div>
+	<div
+		style:transform="translateX(calc({selectedIdx}*100%))"
+		class="-z-10 absolute w-1/2 h-full bg-heading rounded-full transition-[transform] ease-in-out duration-100"
+	></div>
+
+	{#each tabs as tab}
+		<button
+			class:active={selected === tab}
+			class="w-full py-1 cursor-pointer [.active]:cursor-default not-[.active]:hover:text-heading not-[.active]:hover:underline [.active]:font-bold [.active]:text-zinc-950 transition-colors duration-300"
+			onclick={() => onSelect(tab)}>{tab}</button
+		>
+	{/each}
 </div>
