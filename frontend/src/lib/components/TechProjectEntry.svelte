e8aaf722b2acfdb4f27e6429d956b242d56b570e
Mattis Kristensen <mattismoel@gmail.com>
2025-09-01 16:52:04 +0200

    update spacing on tech project entry

--- frontend/src/lib/components/TechProjectEntry.svelte
+++ frontend/src/lib/components/TechProjectEntry.svelte
@@ -16,7 +16,7 @@
 </script>
 
 <div
-	class="@container flex flex-col gap-8 py-16 first:pt-0 last:pb-0"
+	class="@container flex flex-col gap-12 py-16 first:pt-0 last:pb-0"
 	role="presentation"
 >
 	<div class="flex flex-col gap-4 md:flex-row md:justify-between">
