b006094bbe8d8dfd0fe64d6c0b9e282ebdd06628
Mattis Kristensen <mattismoel@gmail.com>
2026-07-12 19:52:28 +0200

    add login functionality

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,14 +1,19 @@
 <script lang="ts">
-	import "../app.css";
-	import favicon from "$lib/assets/favicon.svg";
-	import Navbar from "$lib/components/Navbar.svelte";
-	import Footer from "$lib/components/Footer.svelte";
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+	import Navbar from '$lib/components/Navbar.svelte';
+	import Footer from '$lib/components/Footer.svelte';
 
 	let { children } = $props();
-	const navEntries = new Map<string, string>([["/", "Home"], ["/contact", "Contact"]]);
+	const navEntries = new Map<string, string>([
+		['/', 'Home'],
+		['/contact', 'Contact']
+	]);
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 <Navbar entries={navEntries} />
-{@render children?.()}
+<div class="min-h-svh grid">
+	{@render children?.()}
+</div>
 <Footer />
