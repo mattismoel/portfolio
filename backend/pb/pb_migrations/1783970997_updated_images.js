caba94d4ba1e5d8df4347fbf74a84ca4a51480a9
Mattis Kristensen <mattismoel@gmail.com>
2026-07-14 12:05:32 +0200

    update migrations

---
+++ backend/pb/pb_migrations/1783970997_updated_images.js
@@ -0,0 +1,44 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3607937828")
+
+  // update field
+  collection.fields.addAt(2, new Field({
+    "autogeneratePattern": "",
+    "help": "",
+    "hidden": false,
+    "id": "text1843675174",
+    "max": 0,
+    "min": 0,
+    "name": "description",
+    "pattern": "",
+    "presentable": true,
+    "primaryKey": false,
+    "required": true,
+    "system": false,
+    "type": "text"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3607937828")
+
+  // update field
+  collection.fields.addAt(2, new Field({
+    "autogeneratePattern": "",
+    "help": "",
+    "hidden": false,
+    "id": "text1843675174",
+    "max": 0,
+    "min": 0,
+    "name": "description",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": true,
+    "system": false,
+    "type": "text"
+  }))
+
+  return app.save(collection)
+})
