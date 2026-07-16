e7b6315eb0c34a098e9e6f94b65d388bb70c9f68
Mattis Kristensen <mattismoel@gmail.com>
2026-07-16 13:45:44 +0200

    improve resposiveness of experience entry

--- frontend/src/lib/features/experience/components/ExperienceEntry.svelte
+++ frontend/src/lib/features/experience/components/ExperienceEntry.svelte
@@ -48,7 +48,7 @@
 	<header class="mb-8 sm:mb-4 @container">
 		<div class="flex justify-between flex-col @md:flex-row gap-4 sm:gap-2">
 			<div>
-				<h1 class="pointer-coarse:text-text-light">{name}</h1>
+				<h1 class="pointer-coarse:text-text-light font-bold sm:font-normal mb-2 sm:mb-1">{name}</h1>
 				<p class="sm:text-xs flex items-center gap-2">
 					<span class="icon-[boxicons--location]"></span>
 					{location}
