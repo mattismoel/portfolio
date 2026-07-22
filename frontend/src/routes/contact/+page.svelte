ce6d486ac152795f29ea47913af84a4cb9ee9564
Mattis Kristensen <mattismoel@gmail.com>
2026-07-22 23:40:47 +0200

    update styling of contact page

--- frontend/src/routes/contact/+page.svelte
+++ frontend/src/routes/contact/+page.svelte
@@ -8,9 +8,9 @@
 	<meta name="description" content="The contact page of Mattis Møl Kristensen" />
 </svelte:head>
 
-<main class="flex flex-col">
+<main class="grid">
 	<div class="flex-1 flex flex-col justify-center items-center gap-4">
-		<span>Catch me here</span>
+		<span class="font-bold text-text-light">Catch me here:</span>
 		<div class="flex flex-col items-center gap-2">
 			<ResponsiveLink href="mailto:mattismoel@gmail.com">
 				<IconContainer icon="icon-[boxicons--envelope]">mattismoel@gmail.com</IconContainer>
