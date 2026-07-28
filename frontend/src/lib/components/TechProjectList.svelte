3f4fbe3933e9502cbbbf3f3814cfb431fb999897
Mattis Kristensen <mattismoel@gmail.com>
2026-07-28 13:38:28 +0200

    import issue (+formatting)

--- frontend/src/lib/components/TechProjectList.svelte
+++ frontend/src/lib/components/TechProjectList.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
-  import type { TechProject } from "$lib/project";
-  import TechProjectEntry from "./TechProjectEntry.svelte";
+	import type { TechProject } from '$lib/features/tech-project/tech-project';
+	import TechProjectEntry from './TechProjectEntry.svelte';
 
-  type Props = {
-    projects: TechProject[];
-  };
+	type Props = {
+		projects: TechProject[];
+	};
 
-  const { projects }: Props = $props();
+	const { projects }: Props = $props();
 </script>
 
 <div class="flex flex-col divide-y divide-zinc-900">
-  {#each projects as project}
-    <TechProjectEntry {project} />
-  {/each}
+	{#each projects as project}
+		<TechProjectEntry {project} />
+	{/each}
 </div>
