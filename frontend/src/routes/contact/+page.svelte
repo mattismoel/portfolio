65da22464fd2f8f189ae86c4c987f9fea21a6e17
Mattis Kristensen <mattismoel@gmail.com>
2026-07-30 15:15:37 +0200

    improve contact page

--- frontend/src/routes/contact/+page.svelte
+++ frontend/src/routes/contact/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import IconContainer from '$lib/components/IconContainer.svelte';
 	import ResponsiveLink from '$lib/components/ResponsiveLink.svelte';
+	import { socials, type Social } from '$lib/personal';
 </script>
 
 <svelte:head>
@@ -9,15 +10,33 @@
 </svelte:head>
 
 <main class="grid">
-	<div class="flex-1 flex flex-col justify-center items-center gap-4">
-		<span class="font-bold text-text-light">Catch me here:</span>
-		<div class="flex flex-col items-center gap-2">
-			<ResponsiveLink href="mailto:mattismoel@gmail.com">
-				<IconContainer icon="icon-[boxicons--envelope]">mattismoel@gmail.com</IconContainer>
-			</ResponsiveLink>
-			<ResponsiveLink href="tel:+4542157066">
-				<IconContainer icon="icon-[boxicons--phone]">+4542157066</IconContainer>
-			</ResponsiveLink>
+	<div class="flex-1 flex flex-col justify-center items-center">
+		<div>
+			<h1 class="font-bold text-text-light mb-4">Catch me here:</h1>
+			<ul class="grid gap-2">
+				{#each socials as social}
+					{@render socialEntry(social)}
+				{/each}
+			</ul>
 		</div>
 	</div>
 </main>
+
+{#snippet socialEntry({ name, url, text, iconName }: Social)}
+	<li class="group relative w-fit">
+		<ResponsiveLink title={name} href={url}>
+			<IconContainer icon={iconName}>
+				{name}
+			</IconContainer>
+		</ResponsiveLink>
+
+		<IconContainer
+			icon="icon-[boxicons--arrow-right]"
+			spacing="tight"
+			iconClass="size-6"
+			class="absolute text-text-light/30 opacity-0 right-0 group-hover:-right-2 top-1/2 translate-x-full -translate-y-1/2 group-hover:opacity-100 transition-[right,opacity]"
+		>
+			<p class="whitespace-nowrap">{text}</p>
+		</IconContainer>
+	</li>
+{/snippet}
